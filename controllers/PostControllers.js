d36d4280a82872593fc5ad688cfafa4458213aa5
Abir Santra <santraabir8@gmail.com>
2022-08-12 18:37:29 +0530

    Added updatePost route and logic

--- controllers/PostControllers.js
+++ controllers/PostControllers.js
@@ -25,3 +25,36 @@ export const createPost = async (req, res) => {
   5. Else, send error message
   */
 };
+
+//! Update a post
+export const updatePost = async (req, res) => {
+  const targetPostId = req.params.id;
+
+  const { userId } = req.body;
+
+  try {
+    const prevPost = await postModel.findById(targetPostId);
+    if (prevPost.userId === userId) {
+      await prevPost.updateOne({ $set: req.body });
+      res.status(200).json("Post Updated successfully!");
+    } else {
+      res
+        .status(403)
+        .json("Action forbidden! You can only update your own post!");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+  /*
+  1. Get the target post id from the params
+  2. Get the userId from the body
+  3. Find the target post from the database using the target post id
+  4. Check if the targetPost's userId field matches with the userId provided in the body
+  5. If true, then that means the user is trying to update his own post which is acceptable
+  6. Update the targetPost using updateOne() and $set the post to be req.body
+  7. Send the success messsage.
+  8. Else, it means the user is trying to update someone else's post which is not acceptable. So send error message.
+  9. If any error, send error message.
+  */
+};
--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,12 +1,14 @@
 //! This file contains all the routes related to Posts
 
 import express from "express";
-import { createPost } from "../controllers/PostControllers.js";
+import { createPost, updatePost } from "../controllers/PostControllers.js";
 
 //Creating the router object
 const router = express.Router();
 
 router.post("/", createPost); //-> Creating a post
+
+router.put("/:id", updatePost); //-> Update a post
 
 //Exporting the router object
 export default router;
