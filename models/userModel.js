057d0fd5e3e830f4373f5b7f4e674b88c4399331
Abir Santra <santraabir8@gmail.com>
2022-10-11 18:18:59 +0530

    added donation link to schema

--- models/userModel.js
+++ models/userModel.js
@@ -4,43 +4,44 @@ import mongoose from "mongoose";
 
 // User Schema Definition
 const UserSchema = mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    firstname: {
-      type: String,
-      required: true,
-    },
-    lastname: {
-      type: String,
-      required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-    profilePhoto: String,
-    coverPhoto: String,
-    bio: String,
-    livesIn: String,
-    worksAt: String,
-    skills: String,
-    followers: [],
-    following: [],
-    savedPosts: [],
-  },
-  { timestamps: true }
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		firstname: {
+			type: String,
+			required: true,
+		},
+		lastname: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
+		profilePhoto: String,
+		coverPhoto: String,
+		bio: String,
+		livesIn: String,
+		worksAt: String,
+		skills: String,
+		followers: [],
+		following: [],
+		savedPosts: [],
+		donationLink: String,
+	},
+	{ timestamps: true }
 );
 
 // User Model Definition
