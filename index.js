dd4742c823ee26f69091ec6b5d6a3e63c3bfbb16
Abir Santra <santraabir8@gmail.com>
2022-10-11 18:16:38 +0530

    Added Stripe

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import AuthRoute from "./routes/AuthRoute.js";
 import UserRoute from "./routes/UserRoute.js";
 import PostRoute from "./routes/PostRoute.js";
+import Stripe from "stripe";
 
 // Initializing the app server
 const app = express();
@@ -18,18 +19,21 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 dotenv.config();
 
+// Stripe Payments
+const stripe = new Stripe(process.env.STRIPE_SECRET);
+
 // MongoDB Atlas Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Server started at port ${process.env.PORT}`)
-    )
-  )
-  .catch((error) => console.log(error));
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() =>
+		app.listen(process.env.PORT, () =>
+			console.log(`Server started at port ${process.env.PORT}`)
+		)
+	)
+	.catch((error) => console.log(error));
 
 //Routes
 app.use("/auth", AuthRoute); //-> Authentication Routes
