d97d91febb513a9e8cf7e37a1858114c81e15117
Abir Santra <santraabir8@gmail.com>
2022-12-28 21:43:14 +0530

    added logout and refresh route

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -2,12 +2,21 @@
 // All routes start with /auth
 
 import express from "express";
-import { loginUser, registerUser } from "../controllers/AuthControllers.js";
+import {
+	loginUser,
+	registerUser,
+	logout,
+	refresh,
+} from "../controllers/AuthControllers.js";
 
 const router = express.Router(); //create a router object
 
 router.post("/register", registerUser); //Route for registering a user
 
 router.post("/login", loginUser); //Route for logining in a user
+
+router.get("/refresh", refresh); // Route for refreshing the access token
+
+router.get("/logout", logout); // Route for logging out user
 
 export default router;
