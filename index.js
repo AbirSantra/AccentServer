c3098ee3e0dbf3f8ed347ad9edc7b1ff1c0d8665
Abir Santra <santraabir8@gmail.com>
2022-12-28 18:48:39 +0530

    added cookie-parser middleware

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ import cors from "cors";
 import AuthRoute from "./routes/AuthRoute.js";
 import UserRoute from "./routes/UserRoute.js";
 import PostRoute from "./routes/PostRoute.js";
-// import Stripe from "stripe";
+import cookieParser from "cookie-parser";
 
 // Initializing the app server
 const app = express();
+dotenv.config();
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.text({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-dotenv.config();
+app.use(cookieParser());
 
 // MongoDB Atlas Connection
 mongoose
