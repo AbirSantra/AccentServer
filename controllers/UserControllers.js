8c75a7f5868ce7c4844c1c3ce5465bc7a7c21dd3
Abir Santra <santraabir8@gmail.com>
2023-01-04 02:11:55 +0530

    refactored user controllers

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,5 +1,4 @@
 //! This file contains all the controllers for all the routes related to users
-//? These are basically the functions that are carried out when the specific route is called
 
 import userModel from "../models/userModel.js";
 import bycrpt from "bcrypt";
@@ -60,9 +59,11 @@ export const updateUser = async (req, res) => {
 
 	const { currentUserId, isAdmin, password, ...userDetails } = req.body;
 
+	// try to find a user with the same username
 	const existingUser = await userModel.findOne({ username: req.body.username });
 
 	if (existingUser) {
+		// if the existing user is not the current user
 		if (existingUser?._id?.toString() !== currentUserId) {
 			return res.status(400).json({ message: "Username is already taken!" });
 		}
@@ -84,22 +85,13 @@ export const updateUser = async (req, res) => {
 			);
 			res.status(200).json(updatedUser);
 		} catch (error) {
-			res.status(500).json(error);
+			res.status(500).json(error.message);
 		}
 	} else {
 		res
 			.status(403)
 			.json("Access Denied! You can only update your own profile.");
 	}
-
-	// Get the userId of the account we want to update from params
-	// Get the current user's id, admin status, and updated password from the req.body
-	// A user can only update an account if (1) it is his own account, that is, the target user id and current user id are the same or (2) if the current user is an admin
-	// Check if the request contains a password field
-	// If true, then that means that the user has requested a password changed. So we change the password field in the req.body to a hashed password
-	// Then, update the user details using findByIdAndUpdate() passing the target id, req.body(details to be updated) and set new:true
-	// If any error occurs, return the error message.
-	// Incase, the current user id does not match the target id or the current user is not admin, return message "Access denied"
 };
 
 //! Deleting a user
@@ -120,13 +112,6 @@ export const deleteUser = async (req, res) => {
 			.status(403)
 			.json("Access denied! You can only delete your own profile.");
 	}
-
-	// Get the target user id to be deleted from the params
-	// Get the current user id, admin status from the req.body
-	// A user can delete an account only if (1) the user is trying to delete his own account, that is, the current user id is the same as the target user id or (2) the current user is an admin
-	// Delete the user account using findByIdAndDelete() and send successful completion message
-	// If any error, send the error message
-	// In case, the current user id does not match the target user id or the current user is not an admin, return message "Access Denied"
 };
 
 //! Following a User
@@ -135,6 +120,7 @@ export const followUser = async (req, res) => {
 
 	const { currentUserId } = req.body;
 
+	// check if user is trying following himself
 	if (targetUserId === currentUserId) {
 		res
 			.status(403)
@@ -155,19 +141,6 @@ export const followUser = async (req, res) => {
 			res.status(500).json(error.message);
 		}
 	}
-
-	// Get the target user id from the params
-	// Get the current user id from the body
-	// Check if the current user id and the target user id are the same.
-	// If true, then it means the user is trying to follow themselves. Return action forbidden message
-	// Else, proceed
-	// Get the target user details from the database using findById()
-	// Get the current user details from the database using findById()
-	// If the targetUser.follower array does not include the current user id, then proceed
-	// Push the currentUserId into the followers array of the targetUser using updateOne() and $push
-	// Push the targetUserId into the followings array of the currentUser using updateOne() and $push
-	// Send success message
-	// Else, send error message
 };
 
 //! Unfollow User
@@ -176,6 +149,7 @@ export const unfollowUser = async (req, res) => {
 
 	const { currentUserId } = req.body;
 
+	// check if user if trying to unfollow himself
 	if (targetUserId === currentUserId) {
 		res.status(403).json("Action Forbidden! You cannot unfollow yourself");
 	} else {
@@ -195,19 +169,6 @@ export const unfollowUser = async (req, res) => {
 			res.status(500).json(error);
 		}
 	}
-
-	// Get the target user id from the params
-	// Get the current user id from the body
-	// Check if the current user id and the target user id are the same.
-	// If true, then it means the user is trying to follow themselves. Return action forbidden message
-	// Else, proceed
-	// Get the target user details from the database using findById()
-	// Get the current user details from the database using findById()
-	// If the targetUser.follower array does include the current user id, then proceed
-	// Pull the currentUserId into the followers array of the targetUser using updateOne() and $pull
-	// Pull the targetUserId into the followings array of the currentUser using updateOne() and $pull
-	// Send success message
-	// Else, send error message
 };
 
 //! Get the followers of an user
@@ -215,12 +176,13 @@ export const getUserFollowers = async (req, res) => {
 	const currentUserId = req.params.id;
 
 	try {
+		// get the current user
 		const currentUser = await userModel.findById(currentUserId);
-		const followers = await Promise.all(
-			currentUser.followers.map((followerId) => {
-				return userModel.findById(followerId);
-			})
-		);
+
+		// get all the users that are present in the current users followers array
+		const followers = await userModel.find({
+			_id: { $in: currentUser.followers },
+		});
 
 		res.status(200).json(followers);
 	} catch (error) {
@@ -233,12 +195,13 @@ export const getUserFollowings = async (req, res) => {
 	const currentUserId = req.params.id;
 
 	try {
+		// get the current user
 		const currentUser = await userModel.findById(currentUserId);
-		const followings = await Promise.all(
-			currentUser.following.map((followingId) => {
-				return userModel.findById(followingId);
-			})
-		);
+
+		// get all the users that are present in the followers array of the user
+		const followings = await userModel.find({
+			_id: { $in: currentUser.following },
+		});
 
 		res.status(200).json(followings);
 	} catch (error) {
